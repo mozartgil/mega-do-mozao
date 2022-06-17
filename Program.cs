a233efec680d155fd9302547aef3497d56e220f4
Mozart Gil <mozartgil@gmail.com>
2022-06-17 12:20:11 -0300

    Numeros do Cliente e Números da Mega Sorteados
    Números do cliente escolhidos e suas devidas validações // Números da Mega sorteados // Mensagens de premiações desenvolvidas

--- Program.cs
+++ Program.cs
@@ -2,28 +2,58 @@
 class MegaDoMozao {
     public static void Main() {
 
-        var numeroMinimo = 1;
-        var numeroMaximo = 60;
-        var numeroParaEscolher = 6;
+        var numeroMinimo = 1; //Número mínimo da cartela
+        var numeroMaximo = 60; //Número máximo da cartela
+        var numeroParaEscolher = 6; //Números a serem escolhidos pelo cliente
         var jogoA = new List<Int32>();
+        var numeroValido = false; //Valida se o INPUT é um número válido 
+        var numeroOrdinal = ""; //Transcrição do número ordinal, Primeiro, Segundo, Terceiro...
+        var numeroEscolhido = 0; //Número INPUTADO pelo cliente
 
+        //Gerando números Aleatórios da Mega
+        var random = new Random();
+        var numerosSorteados = Enumerable.Range(0,6)
+                                    .Select(numero => random.Next(1, 60)).ToList();
+
+        foreach (var item in numerosSorteados)
+        {
+            Console.WriteLine($"{item} - ");
+            //jogoA.Add(item);
+        }
+
+        //Usuário escolhe os 6 números da cartela
         for (int item = 1; item < numeroParaEscolher+1; item++) {
-            var numeroOrdinal = ConverterNumeroOrdinal(item);
+            numeroOrdinal = ConverterNumeroOrdinal(item);
+            numeroValido = false;
+
+            while (!numeroValido)
+            {
+                Console.Write($"Escolha o {numeroOrdinal} número válido entre {numeroMinimo} e {numeroMaximo}: ");
+                var strNumeroEscolhido = Console.ReadLine();
+                // var strNumeroEscolhido = "22";
             
-            Console.Write($"Escolha o {numeroOrdinal} número válido entre {numeroMinimo} e {numeroMaximo}: ");
-            var strNumeroEscolhido = Console.ReadLine();
-            //var strNumeroEscolhido = "76";
-            
-            while (!ValidaNumeroEscolhido(numeroMinimo, numeroMaximo, strNumeroEscolhido)) {
-                Console.Write($"Por favor, escolha um número válido entre {numeroMinimo} e {numeroMaximo}: ");
-                strNumeroEscolhido = Console.ReadLine();
-                //strNumeroEscolhido = "41";
+                while (!ValidaNumeroEscolhido(numeroMinimo, numeroMaximo, strNumeroEscolhido)) {
+                    Console.Write($"Por favor, escolha um número válido entre {numeroMinimo} e {numeroMaximo}: ");
+                    strNumeroEscolhido = Console.ReadLine();
+                    //strNumeroEscolhido = "41";
+                }
+
+                numeroEscolhido = int.Parse(strNumeroEscolhido);
+
+                //Validando se o número escolhido já não foi escolhido antes
+                if (jogoA.Contains(numeroEscolhido))
+                {
+                    Console.WriteLine($"O número {numeroEscolhido} já foi escolhido. Por favor escolha outro!");
+                    numeroValido = false;
+                } else {
+                    numeroValido = true;
+                }   
             }
-
-            var numeroEscolhido = int.Parse(strNumeroEscolhido);
-
+            
             jogoA.Add(numeroEscolhido);
-        }        
+        }    
+
+
         Console.WriteLine("---------");
         Console.WriteLine("Números escolhidos! Muito obrigado.");
         Console.WriteLine("---------");
@@ -32,7 +62,29 @@ class MegaDoMozao {
         {
             Console.WriteLine(numero);
         }
+        
+        //Validando quantos números foram acertados
+        int quantidadeNumerosAcertados = 0;
 
+        foreach (var numero in jogoA)
+        {
+            if (numerosSorteados.Contains(numero)) {
+                quantidadeNumerosAcertados++;
+            }
+        }
+
+        //Mensagens das premiaçÕes
+        if (quantidadeNumerosAcertados == 0) {
+            Console.WriteLine("Infelizmente não foi desta vez. Tente novamente");
+        } else if (quantidadeNumerosAcertados >= 1 && quantidadeNumerosAcertados <= 3) {
+            Console.WriteLine($"Você acertou {quantidadeNumerosAcertados} números mas infelizmente não foi desta vez. Tente novamente");
+        } else if (quantidadeNumerosAcertados == 4) {
+            Console.WriteLine("PARABÉNS! Você acertou uma QUADRA!!!");
+        } else if (quantidadeNumerosAcertados == 5) {
+            Console.WriteLine("PARABÉNS! Você acertou uma QUINA!!!");
+        } else if (quantidadeNumerosAcertados == 6) {
+            Console.WriteLine("PARABÉNS! VOCÊ GANHOU NA MEGA DO MOZÃO!!!");
+        }
     }
 
     public static bool ValidaNumeroEscolhido(Int32 numeroMinimo, Int32 numeroMaximo, string strNumeroEscolhido) {
